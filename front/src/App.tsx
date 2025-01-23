1b22400b4d4f50a301c0a15cfc781e39b2b3e0d7
Anton Rum <antonrumancev67@gmail.com>
2025-01-23 16:20:31 +0300

    Add routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import TasksPage from './Pages/TasksPage.tsx';
+import LoginPage from './Pages/LoginPage.tsx';
+import AuthPage from './Pages/AuthPage.tsx';
+import ProfilePage from './Pages/ProfilePage.tsx';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<TasksPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/auth" element={<AuthPage />}/>
+        <Route path="/profile" element={<ProfilePage />}/>
+      </Routes>
     </div>
   );
 }
---
+++ front/src/Pages/TasksPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TasksPage = (props: Props) => {
+  return (
+    <div>TasksPage</div>
+  )
+}
+
+export default TasksPage
