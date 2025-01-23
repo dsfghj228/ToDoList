89d191fc513767658a4b41efc1906fbf2d324a7e
Anton Rum <antonrumancev67@gmail.com>
2025-01-23 17:50:12 +0300

    add Auth service

---
+++ front/src/Services/AuthService.tsx
@@ -0,0 +1,36 @@
+import axios from "axios"
+
+const api = "http://localhost:7197/api";
+
+type UserProfileToken = {
+    userName: string;
+    email: string;
+    token: string;
+}
+
+export const loginAPI = async (username: string, password: string) => {
+    try {
+        const data = await axios.post<UserProfileToken>(api + "account/login", {
+            username: username,
+            password: password
+        })
+
+        return data;
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const registerAPI = async (email: string, username: string, password: string) => {
+    try {
+        const data = await axios.post<UserProfileToken>(api + "account/login", {
+            email: email,
+            username: username,
+            password: password
+        })
+
+        return data;
+    } catch (err) {
+        console.log(err)
+    }
+}
